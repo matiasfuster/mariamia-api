f874f04b528fda5ce41a41ee7450faff7c16cb50
agustin-tabarez/repository-github <agustin.tabarez@solcre.com>
2017-08-21 18:39:39 -0300

    add solcre

---
+++ public/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <input type="text" id="username" name="username" placeholder="Ingresar usuario">
+            <input type="password" id="password" name="password" placeholder="Ingresar contraseña">
+            <button type="submit">Acceder</button>
+        </form> 
+    </body>
+</html>
+
